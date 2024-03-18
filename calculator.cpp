b2bb827bdc4a1678f4b89d03e2637abfec62f6c8
Izat1966 <150505243+Izat1966@users.noreply.github.com>
2024-03-18 11:52:37 +0600

    calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -7,27 +7,53 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <iostream>
-
+#include <string>
+#include <sstream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int op1,op2,res;
-    char c;
-    cin>>op1>>c>>op2;
-    if (c=='+')
-    res=op1+op2;
-    if (c=='-')
-    res=op1-op2;
-    if(c=='/'){
-        res=op1/op2;
-    }else if (op2==0){
-        return 1;
+    string expression;
+    cout << "Welcome to the calculator" << "\n" << "Please enter your arithmetic expression: ";
+    getline(cin, expression);
+
+    istringstream iss(expression);
+    int result = 0;
+    string op;
+    iss >> result;
+
+    while (iss >> op) {
+        int operand;
+        iss >> operand;
+
+        if (op == "+") {
+            result += operand;
+        } else if (op == "-") {
+            result -= operand;
+        } else if (op == "*") {
+            result *= operand;
+        } else if (op == "/") {
+            if (operand == 0) {
+                cout << "Error: Division by zero is not allowed.";
+                return 1;
+            }
+            result /= operand;
+        }else if (op == "pow") {
+            result
+        }    
+            else {
+            cout << "Error: Invalid operator entered.";
+            return 1;
+        }
     }
-    if(c=='*')
-    res=op1*op2;
-    cout<<res;
+    string ans;
+    cout << "Result: " << result << endl;
+    cout << "Do you want to continue or no (Y/N): ";
+    cin >> ans;
+    if (ans=='Y' || 'y') {
+        
+    }
 
     return 0;
 }
-
