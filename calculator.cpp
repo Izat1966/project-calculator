28b02a0fce29dd3d2e7113f10e978b3b3ddc7f6f
Izat1966 <150505243+Izat1966@users.noreply.github.com>
2024-03-18 15:21:50 +0600

    calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,7 +5,6 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -14,46 +13,44 @@ using namespace std;
 
 int main()
 {
-    string expression;
-    cout << "Welcome to the calculator" << "\n" << "Please enter your arithmetic expression: ";
-    getline(cin, expression);
+    char ans;
+    do {
+        string expression;
+        cout << "Welcome to the calculator" << "\n" << "Please enter your arithmetic expression: ";
+        getline(cin, expression);
 
-    istringstream iss(expression);
-    int result = 0;
-    string op;
-    iss >> result;
+        istringstream iss(expression);
+        int result = 0;
+        char op;
+        iss >> result;
 
-    while (iss >> op) {
-        int operand;
-        iss >> operand;
+        while (iss >> op) {
+            int operand;
+            iss >> operand;
 
-        if (op == "+") {
-            result += operand;
-        } else if (op == "-") {
-            result -= operand;
-        } else if (op == "*") {
-            result *= operand;
-        } else if (op == "/") {
-            if (operand == 0) {
-                cout << "Error: Division by zero is not allowed.";
+            if (op == '+') {
+                result += operand;
+            } else if (op == '-') {
+                result -= operand;
+            } else if (op == '*') {
+                result *= operand;
+            } else if (op == '/') {
+                if (operand == 0) {
+                    cout << "Error: Division by zero is not allowed.";
+                    return 1;
+                }
+                result /= operand;
+            } else {
+                cout << "Error: Invalid operator entered.";
                 return 1;
             }
-            result /= operand;
-        }else if (op == "pow") {
-            result
-        }    
-            else {
-            cout << "Error: Invalid operator entered.";
-            return 1;
         }
-    }
-    string ans;
-    cout << "Result: " << result << endl;
-    cout << "Do you want to continue or no (Y/N): ";
-    cin >> ans;
-    if (ans=='Y' || 'y') {
-        
-    }
+
+        cout << "Result: " << result << endl;
+        cout << "Do you want to continue (Y/N): ";
+        cin >> ans;
+        cin.ignore();
+    } while (ans == 'Y' || ans == 'y');
 
     return 0;
 }
